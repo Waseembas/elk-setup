39bb6745859fee9f163bfdbf689a53017720a5d6
M.waseem <waseem.khalid@digitify.com>
2022-03-16 11:25:36 +0500

    remove logstash gz

---
+++ docker-compose/kibana/password.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Configure Bootstrap Password
+
+BootstrapPassword="Test@1234"
+
+while true
+do
+  curl --cacert /usr/share/kibana/certs/ca/ca.crt --fail -u "elastic:$BootstrapPassword" \
+    "https://master:9200/_cluster/health?wait_for_status=yellow" \
+    && break
+  sleep 5
+done
+
+# Set passwords for various users
+for User in "kibana" "logstash_system" "apm_system" "beats_system" "elastic"
+do
+  UserPassword=Test@1234
+  curl --cacert /usr/share/kibana/certs/ca/ca.crt -u "elastic:${BootstrapPassword}" \
+    -XPOST "https://master:9200/_xpack/security/user/${User}/_password" \
+    -d'{"password":"'"${UserPassword}"'"}' -H "Content-Type: application/json"
+  printf "%s=%s\n" "$User" "$UserPassword" >> passwords.txt
+done
+cat passwords.txt
+./bin/kibana
+
